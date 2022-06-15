9215ebef69b7b406a5f484b838867c2e9c49a750
Ben Devlin <bdevlin@gmail.com>
2022-06-14 23:22:18 -0400

    Basic files for running vivado synth and reporting utilization

---
+++ scripts/run_vivado.tcl
@@ -0,0 +1,15 @@
+if { $argc != 1 } {
+    puts "The script requires input top level module name."
+    exit
+}
+set application [lindex $argv 0]
+
+create_project -part xcvu9p-flga2104-3-e -in_memory 
+add_files ${application}.v
+add_files ${application}.xdc
+
+set_property top ${application} [current_fileset]
+
+synth_design -mode out_of_context
+
+report_utilization -hierarchical -file post_synth_utilization.rpt
