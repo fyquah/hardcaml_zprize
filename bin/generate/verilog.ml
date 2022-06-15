6b20e96836c9d96b38c0a032f84dc9b17cab21c6
Ben Devlin <bdevlin@gmail.com>
2022-06-14 21:58:48 -0400

    Add command for point double

--- bin/generate/verilog.ml
+++ bin/generate/verilog.ml
@@ -7,79 +7,85 @@ include struct
   module Ark_bls12_377_g1 = Ark_bls12_377_g1
 end
 
-module Karatsuba_ofman_mult377 = Karatsuba_ofman_mult.With_interface(struct
-    let bits = 377
-  end)
-module Montgometry_mult377 = Montgomery_mult.With_interface(struct
-    let bits = 377
-  end)
+module Karatsuba_ofman_mult377 = Karatsuba_ofman_mult.With_interface (struct
+  let bits = 377
+end)
+
+module Montgometry_mult377 = Montgomery_mult.With_interface (struct
+  let bits = 377
+end)
+
+module Ec_fpn_dbl = Ec_fpn_dbl.With_interface (struct
+  let bits = 377
+end)
 
 let flag_filename =
   Command.Param.(
-    flag "output-filename"
-      (optional_with_default "/dev/stdout" string) ~doc:" Output filename (Defaults to stdout)"
-  )
+    flag
+      "output-filename"
+      (optional_with_default "/dev/stdout" string)
+      ~doc:" Output filename (Defaults to stdout)")
 ;;
 
 let flag_multiplier_config =
   let%map_open.Command depth =
-    flag "depth" (optional_with_default 4 int)
-      ~doc:" Depth karatsuba ofman splitting"
+    flag "depth" (optional_with_default 4 int) ~doc:" Depth karatsuba ofman splitting"
   and multiplier_latency =
-    flag "multiplier-latency" (optional_with_default 1 int)
+    flag
+      "multiplier-latency"
+      (optional_with_default 1 int)
       ~doc:" Latency of ground multipliers"
   and use_vanila_multiply =
-    flag "use-vanila-multiply" no_arg
+    flag
+      "use-vanila-multiply"
+      no_arg
       ~doc:" Use vanila verilog multplication, rather than optimized hybrid"
   in
   Karatsuba_ofman_mult.Config.generate
-    ~ground_multiplier:(
-      if use_vanila_multiply then
-        Verilog_multiply { latency = multiplier_latency }
-      else
-        Hybrid_dsp_and_luts { latency = multiplier_latency }
-    )
+    ~ground_multiplier:
+      (if use_vanila_multiply
+      then Verilog_multiply { latency = multiplier_latency }
+      else Hybrid_dsp_and_luts { latency = multiplier_latency })
     ~depth
 ;;
 
 let command_karatsuba_ofman_mult =
-  Command.basic ~summary:"karatsuba-ofman-mult"
+  Command.basic
+    ~summary:"karatsuba-ofman-mult"
     (let%map_open.Command config = flag_multiplier_config
-     and filename = flag_filename
-     in
-      fun () ->
+     and filename = flag_filename in
+     fun () ->
        let module K = Karatsuba_ofman_mult377 in
-       let module C = Circuit.With_interface(K.I)(K.O) in
+       let module C = Circuit.With_interface (K.I) (K.O) in
        let scope = Scope.create () in
        let database = Scope.circuit_database scope in
-       let circuit =
-         C.create_exn ~name:"karatsuba_ofman_mult" (K.create ~config scope)
-       in
+       let circuit = C.create_exn ~name:"karatsuba_ofman_mult" (K.create ~config scope) in
        Rtl.output ~database ~output_mode:(To_file filename) Verilog circuit)
 ;;
 
 let command_montgomery_mult =
-  Command.basic ~summary:"montgomery-mult"
+  Command.basic
+    ~summary:"montgomery-mult"
     (let%map_open.Command multiplier_config = flag_multiplier_config
      and adder_depth =
-       flag "adder-depth" (optional_with_default 3 int)
+       flag
+         "adder-depth"
+         (optional_with_default 3 int)
          ~doc:" Depth of adder stage in montgomery mult. Defaults to 3"
      and subtractor_depth =
-       flag "subtractor-depth" (optional_with_default 3 int)
+       flag
+         "subtractor-depth"
+         (optional_with_default 3 int)
          ~doc:" Depth of subtractor in montgomery mult. Defaults to 3"
-     and filename = flag_filename
-     in
-      fun () ->
+     and filename = flag_filename in
+     fun () ->
        let module M = Montgometry_mult377 in
-       let module C = Circuit.With_interface(M.I)(M.O) in
+       let module C = Circuit.With_interface (M.I) (M.O) in
        let scope = Scope.create ~flatten_design:false () in
        let database = Scope.circuit_database scope in
        let circuit =
          M.create
-           ~config:{ multiplier_config
-                   ; adder_depth
-                   ; subtractor_depth
-                   }
+           ~config:{ multiplier_config; adder_depth; subtractor_depth }
            ~p:(Ark_bls12_377_g1.modulus ())
            scope
          |> C.create_exn ~name:"montgomery_mult"
@@ -87,9 +93,51 @@ let command_montgomery_mult =
        Rtl.output ~database ~output_mode:(To_file filename) Verilog circuit)
 ;;
 
+let command_point_double =
+  Command.basic
+    ~summary:"point double"
+    (let%map_open.Command latency =
+       flag "latency" (optional_with_default 1 int) ~doc:" Latency"
+     and multiplier_config = flag_multiplier_config
+     and adder_depth =
+       flag
+         "adder-depth"
+         (optional_with_default 3 int)
+         ~doc:" Depth of adder stage in montgomery mult. Defaults to 3"
+     and subtractor_depth =
+       flag
+         "subtractor-depth"
+         (optional_with_default 3 int)
+         ~doc:" Depth of subtractor in montgomery mult. Defaults to 3"
+     and filename = flag_filename in
+     fun () ->
+       let module M = Ec_fpn_dbl in
+       let module C = Circuit.With_interface (M.I) (M.O) in
+       let scope = Scope.create ~flatten_design:false () in
+       let database = Scope.circuit_database scope in
+       let p = Ark_bls12_377_g1.modulus () in
+       let multiplier ~scope ~clock ~enable x y =
+         let module M = Montgometry_mult377 in
+         let o =
+           M.create
+             ~config:{ multiplier_config; adder_depth; subtractor_depth }
+             ~p
+             scope
+             { clock; enable; x; y; valid = Signal.vdd }
+         in
+         o.z
+       in
+       let circuit =
+         M.create ~config:{ fp_multiply = { latency; impl = multiplier }; p } scope
+         |> C.create_exn ~name:"point_double"
+       in
+       Rtl.output ~database ~output_mode:(To_file filename) Verilog circuit)
+;;
+
 let () =
   [ "karatsuba-ofman-mult", command_karatsuba_ofman_mult
   ; "montgomery-mult", command_montgomery_mult
+  ; "point-dbl", command_point_double
   ]
   |> Command.group ~summary:"generate verilog"
   |> Command_unix.run
