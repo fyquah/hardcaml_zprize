10ee8495e92b8067b0be8c0f85de8e118416df12
andrewray <evilkidder@gmail.com>
2022-06-18 20:36:42 +0100

    in progress...

--- ntt/src/gf.ml
+++ ntt/src/gf.ml
@@ -2,113 +2,176 @@ open! Base
 open! Hardcaml
 open! Bits
 
-type nonrec t = t
+module Make (Bits : Comb.S) = struct
+  open Bits
 
-let num_bits = 64
-let zero = of_int ~width:num_bits 0
-let one = of_int ~width:num_bits 1
-let two = of_int ~width:num_bits 2
-let epsilon = of_int ~width:num_bits ((1 lsl 32) - 1)
-let modulus = of_int64 ~width:num_bits 0xFFFF_FFFF_0000_0001L
-let mult_mask = of_int64 ~width:num_bits 0xFFFF_FFFF_FFFF_FFFFL
+  type nonrec t = t
 
-let omega =
-  [| "1"
-   ; (* for a domain of 2^0 *)
-     "18446744069414584320"
-   ; (* for a domain of 2^1 *)
-     "281474976710656"
-   ; (* for a domain of 2^2 *)
-     "18446744069397807105"
-   ; (* for a domain of 2^3 *)
-     "17293822564807737345"
-   ; (* for a domain of 2^4 *)
-     "70368744161280"
-   ; (* for a domain of 2^5 *)
-     "549755813888"
-   ; (* for a domain of 2^6 *)
-     "17870292113338400769"
-   ; (* for a domain of 2^7 *)
-     "13797081185216407910"
-   ; (* for a domain of 2^8 *)
-     "1803076106186727246"
-   ; (* for a domain of 2^9 *)
-     "11353340290879379826"
-   ; (* for a domain of 2^10 *)
-     "455906449640507599"
-   ; (* for a domain of 2^11 *)
-     "17492915097719143606"
-   ; (* for a domain of 2^12 *)
-     "1532612707718625687"
-   ; (* for a domain of 2^13 *)
-     "16207902636198568418"
-   ; (* for a domain of 2^14 *)
-     "17776499369601055404"
-   ; (* for a domain of 2^15 *)
-     "6115771955107415310"
-   ; (* for a domain of 2^16 *)
-     "12380578893860276750"
-   ; (* for a domain of 2^17 *)
-     "9306717745644682924"
-   ; (* for a domain of 2^18 *)
-     "18146160046829613826"
-   ; (* for a domain of 2^19 *)
-     "3511170319078647661"
-   ; (* for a domain of 2^20 *)
-     "17654865857378133588"
-   ; (* for a domain of 2^21 *)
-     "5416168637041100469"
-   ; (* for a domain of 2^22 *)
-     "16905767614792059275"
-   ; (* for a domain of 2^23 *)
-     "9713644485405565297"
-   ; (* for a domain of 2^24 *)
-     "5456943929260765144"
-   ; (* for a domain of 2^25 *)
-     "17096174751763063430"
-   ; (* for a domain of 2^26 *)
-     "1213594585890690845"
-   ; (* for a domain of 2^27 *)
-     "6414415596519834757"
-   ; (* for a domain of 2^28 *)
-     "16116352524544190054"
-   ; (* for a domain of 2^29 *)
-     "9123114210336311365"
-   ; (* for a domain of 2^30 *)
-     "4614640910117430873"
-   ; (* for a domain of 2^31 *)
-     "1753635133440165772" (* for a domain of 2^32 *)
-  |]
-  |> Array.map ~f:(fun const ->
-         (* XXX aray: Assert the constants really fit in 64 bits. *)
-         Z.of_string const |> of_z ~width:num_bits)
-;;
+  let sexp_of_t t =
+    Bits.to_z ~signedness:Unsigned t |> Z.to_string |> [%sexp_of: String.t]
+  ;;
 
-let overflow res = mux2 (msb res) Uop.(lsbs res +: epsilon) res
+  let num_bits = 64
+  let zero = of_int ~width:num_bits 0
+  let one = of_int ~width:num_bits 1
+  let two = of_int ~width:num_bits 2
+  let epsilon = of_int ~width:num_bits ((1 lsl 32) - 1)
+  let modulus = of_int64 ~width:num_bits 0xFFFF_FFFF_0000_0001L
+  let mult_mask = of_int64 ~width:num_bits 0xFFFF_FFFF_FFFF_FFFFL
+  let of_int x = Bits.of_int ~width:num_bits x
 
-let add left right =
-  let res = Uop.(left +: right) in
-  overflow res |> overflow |> lsbs
-;;
+  let of_z num_bits z =
+    let b = Bits.of_z ~width:num_bits z in
+    if Z.compare z Z.zero < 0
+    then if Z.equal (Bits.to_z ~signedness:Signed b) z then Some b else None
+    else if Z.equal (Bits.to_z ~signedness:Unsigned b) z
+    then Some b
+    else None
+  ;;
 
-let underflow res = mux2 (msb res) Uop.(lsbs res -: epsilon) res
+  let of_z ?(allow_65_bits = false) z =
+    match of_z 64 z with
+    | Some z -> z
+    | None ->
+      if not allow_65_bits
+      then raise_s [%message "GF element out of range"]
+      else (
+        match of_z 65 z with
+        | Some z -> z
+        | None -> raise_s [%message "GF element out of range"])
+  ;;
 
-let sub left right =
-  let res = Uop.(left +: right) in
-  underflow res |> underflow |> lsbs
-;;
+  let to_z = Bits.to_z ~signedness:Unsigned
 
-let mul left right =
-  let res = left *: right in
-  let t0 = Uop.(res.:[31, 0] -: res.:[63, 48]) in
-  let t0 = underflow t0 in
-  let t1 = res.:[47, 32] *: epsilon in
-  let final = Uop.(t0 +: t1) in
-  overflow final |> lsbs
-;;
+  let omega =
+    [| "1"
+     ; (* for a domain of 2^0 *)
+       "18446744069414584320"
+     ; (* for a domain of 2^1 *)
+       "281474976710656"
+     ; (* for a domain of 2^2 *)
+       "18446744069397807105"
+     ; (* for a domain of 2^3 *)
+       "17293822564807737345"
+     ; (* for a domain of 2^4 *)
+       "70368744161280"
+     ; (* for a domain of 2^5 *)
+       "549755813888"
+     ; (* for a domain of 2^6 *)
+       "17870292113338400769"
+     ; (* for a domain of 2^7 *)
+       "13797081185216407910"
+     ; (* for a domain of 2^8 *)
+       "1803076106186727246"
+     ; (* for a domain of 2^9 *)
+       "11353340290879379826"
+     ; (* for a domain of 2^10 *)
+       "455906449640507599"
+     ; (* for a domain of 2^11 *)
+       "17492915097719143606"
+     ; (* for a domain of 2^12 *)
+       "1532612707718625687"
+     ; (* for a domain of 2^13 *)
+       "16207902636198568418"
+     ; (* for a domain of 2^14 *)
+       "17776499369601055404"
+     ; (* for a domain of 2^15 *)
+       "6115771955107415310"
+     ; (* for a domain of 2^16 *)
+       "12380578893860276750"
+     ; (* for a domain of 2^17 *)
+       "9306717745644682924"
+     ; (* for a domain of 2^18 *)
+       "18146160046829613826"
+     ; (* for a domain of 2^19 *)
+       "3511170319078647661"
+     ; (* for a domain of 2^20 *)
+       "17654865857378133588"
+     ; (* for a domain of 2^21 *)
+       "5416168637041100469"
+     ; (* for a domain of 2^22 *)
+       "16905767614792059275"
+     ; (* for a domain of 2^23 *)
+       "9713644485405565297"
+     ; (* for a domain of 2^24 *)
+       "5456943929260765144"
+     ; (* for a domain of 2^25 *)
+       "17096174751763063430"
+     ; (* for a domain of 2^26 *)
+       "1213594585890690845"
+     ; (* for a domain of 2^27 *)
+       "6414415596519834757"
+     ; (* for a domain of 2^28 *)
+       "16116352524544190054"
+     ; (* for a domain of 2^29 *)
+       "9123114210336311365"
+     ; (* for a domain of 2^30 *)
+       "4614640910117430873"
+     ; (* for a domain of 2^31 *)
+       "1753635133440165772" (* for a domain of 2^32 *)
+    |]
+    |> Array.map ~f:(fun const ->
+           (* XXX aray: Assert the constants really fit in 64 bits. *)
+           Z.of_string const |> of_z)
+  ;;
 
-let to_canonical x = mux2 Uop.(x >=: modulus) (x -: modulus) x
-let negate x = mux2 (x ==:. 0) x (modulus -: to_canonical x)
-let ( +: ) = add
-let ( -: ) = sub
-let ( *: ) = mul
+  let overflow res = mux2 (msb res) Uop.(lsbs res +: epsilon) res
+
+  let _add left right =
+    let res = Uop.(left +: right) in
+    overflow res |> overflow |> lsbs
+  ;;
+
+  let add left right =
+    let res = Uop.(left +: right) in
+    mux2 (res >=: ue modulus) (res -: ue modulus) res |> lsbs
+  ;;
+
+  let underflow res = mux2 (msb res) Uop.(lsbs res -: epsilon) res
+
+  let _sub left right =
+    let res = Uop.(left -: right) in
+    underflow res |> underflow |> lsbs
+  ;;
+
+  let sub left right =
+    let res = Uop.(left -: right) in
+    mux2 (msb res) (res +: ue modulus) res |> lsbs
+  ;;
+
+  let mul left right =
+    let res = left *: right in
+    let t0 = Uop.(res.:[31, 0] -: res.:[63, 48]) in
+    let t0 = underflow t0 in
+    let t1 = res.:[47, 32] *: epsilon in
+    let final = Uop.(t0 +: t1) in
+    overflow final |> lsbs
+  ;;
+
+  let is_normalized x = Uop.(x <: modulus)
+  let to_bits t = t
+  let to_canonical x = mux2 (is_normalized x) x (x -: modulus)
+  let negate x = mux2 (x ==:. 0) x (modulus -: to_canonical x)
+  let ( +: ) = add
+  let ( -: ) = sub
+  let ( *: ) = mul
+end
+
+module Z = struct
+  type t = Z.t
+
+  let sexp_of_t t = Z.to_string t |> [%sexp_of: String.t]
+
+  (* This is a solinas prime (2^64 - 2^32 + 1) which has a fast modular
+     reduction algorithm. *)
+  let modulus = Z.((one lsl 64) - (one lsl 32) + one)
+  let of_z t = Z.(erem t modulus)
+  let add a b = Z.((a + b) mod modulus)
+  let sub a b = Z.(erem (a - b) modulus)
+  let mul a b = Z.(a * b mod modulus)
+  let ( + ) = add
+  let ( - ) = sub
+  let ( * ) = mul
+  let of_int = Z.of_int
+  let to_z t = t
+end
--- ntt/src/gf.mli
+++ ntt/src/gf.mli
@@ -1,21 +1,45 @@
 open! Base
 open! Hardcaml
 
-type t
+module Make (Bits : Comb.S) : sig
+  type t [@@deriving sexp_of]
 
-(** {2 Constants}*)
+  (** {2 Constants}*)
 
-val zero : t
-val one : t
-val two : t
-val epsilon : t
-val modulus : t
-val mult_mask : t
-val omega : t array
+  val zero : t
+  val one : t
+  val two : t
+  val epsilon : t
+  val modulus : t
+  val mult_mask : t
+  val omega : t array
 
-(** {2 Arithmetic operations} *)
+  (** {2 Arithmetic operations} *)
 
-val ( +: ) : t -> t -> t
-val ( -: ) : t -> t -> t
-val negate : t -> t
-val ( *: ) : t -> t -> t
+  val ( +: ) : t -> t -> t
+  val ( -: ) : t -> t -> t
+  val negate : t -> t
+  val ( *: ) : t -> t -> t
+
+  (** {2 Misc} *)
+
+  val is_normalized : t -> Bits.t
+  val to_bits : t -> Bits.t
+  val of_int : int -> t
+  val of_z : ?allow_65_bits:bool -> Z.t -> t
+  val to_z : t -> Z.t
+end
+
+module Z : sig
+  type t [@@deriving sexp_of]
+
+  (** Solinas prime [2^64 - 2^31 + 1] *)
+  val modulus : t
+
+  val of_z : Z.t -> t
+  val of_int : int -> t
+  val to_z : t -> Z.t
+  val ( + ) : t -> t -> t
+  val ( - ) : t -> t -> t
+  val ( * ) : t -> t -> t
+end
