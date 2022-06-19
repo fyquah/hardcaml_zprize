89c0af21516a45f94bdec825ccf56c3d526ed113
fyquah <fyquah@protonmail.com>
2022-06-19 14:22:50 +0100

    Add squarer circuit based on karatsubma-ofman multiplier

---
+++ src/squarer.ml
@@ -0,0 +1,134 @@
+open Base
+open Hardcaml
+open Signal
+open Reg_with_enable
+
+let post_adder_stages ~depth:_ = 1
+
+module Config = struct
+  type t =
+    { depth : int
+    ; ground_multiplier : Ground_multiplier.Config.t
+    }
+
+  let latency { depth; ground_multiplier } =
+    match depth with
+    | 0 -> Ground_multiplier.Config.latency ground_multiplier
+    | _ ->
+      let slowest_multiplier =
+        Karatsuba_ofman_mult.Config.generate ~ground_multiplier ~depth:(depth - 1)
+        |> Karatsuba_ofman_mult.Config.latency
+      in
+      slowest_multiplier + post_adder_stages ~depth
+  ;;
+end
+
+module Input = Multiplier_input
+
+let rec create_recursive ~scope ~clock ~enable ~(config : Config.t) (x : Signal.t) =
+  let spec = Reg_spec.create ~clock () in
+  let pipeline ~n x = if is_const x then x else pipeline ~enable spec x ~n in
+  match config.depth with
+  | 0 -> Ground_multiplier.create ~clock ~enable ~config:config.ground_multiplier x x
+  | _ ->
+    let post_adder_stages = post_adder_stages ~depth:config.depth in
+    let child_karatsuba_config =
+      Karatsuba_ofman_mult.Config.generate
+        ~ground_multiplier:config.ground_multiplier
+        ~depth:(config.depth - 1)
+    in
+    let child_halfwidth_config = { config with depth = config.depth - 1 } in
+    let create_recursive input =
+      create_recursive ~scope ~clock ~enable ~config:child_halfwidth_config input
+      |> pipeline
+           ~n:
+             (Karatsuba_ofman_mult.Config.latency child_karatsuba_config
+             - Config.latency child_halfwidth_config)
+    in
+    let create_full_multiplier a b =
+      Karatsuba_ofman_mult.hierarchical
+        ~scope
+        ~config:child_karatsuba_config
+        ~enable
+        ~clock
+        a
+        (match b with
+        | Const { signal_id = _; constant } ->
+          `Constant (Bits.to_z ~signedness:Unsigned constant)
+        | _ -> `Signal b)
+    in
+    let w = width x in
+    let hw = (w + 1) / 2 in
+    let top_and_btm_half x = uresize (drop_bottom x hw) hw, sel_bottom x hw in
+    (* ((ta * 2^hw) + ba) * ((tb * 2^hw) + bb)
+     * = ((ua * ub) * 2^(2 * hw)) + (((ua * lb) + (ub * la)) * 2^hw) + (la * lb)
+     *
+     * The upper-halve is zero in a half-width hader, reach reduces the circuit
+     * to the following:
+     *
+     * (((ua * lb) + (ub * la)) * 2^hw) + (la * lb)
+     *
+     * In the special case of squaring numbers, ua = la, ub = lb; we need
+     * only 2 mults rather than 3.
+     *)
+    let x1, x0 = top_and_btm_half x in
+    let x1_times_x0 = uresize (create_full_multiplier x1 x0) (2 * w) in
+    let x1_times_x1 = uresize (create_recursive x1) (2 * w) in
+    let x0_times_x0 = uresize (create_recursive x0) (2 * w) in
+    pipeline
+      ~n:post_adder_stages
+      (sll x1_times_x1 (2 * hw) +: sll x1_times_x0 (hw + 1) +: x0_times_x0)
+;;
+
+module type Width = sig
+  val bits : int
+end
+
+module With_interface (M : Width) = struct
+  include M
+
+  module I = struct
+    type 'a t =
+      { clock : 'a
+      ; enable : 'a
+      ; x : 'a [@bits bits]
+      ; in_valid : 'a
+      }
+    [@@deriving sexp_of, hardcaml]
+  end
+
+  module O = struct
+    type 'a t =
+      { y : 'a [@bits 2 * bits]
+      ; out_valid : 'a
+      }
+    [@@deriving sexp_of, hardcaml]
+  end
+
+  let create ~(config : Config.t) scope { I.clock; enable; x; in_valid } =
+    let spec = Reg_spec.create ~clock () in
+    let y = create_recursive ~scope ~clock ~enable ~config x in
+    let out_valid = pipeline spec ~enable ~n:(Config.latency config) in_valid in
+    assert (width y = 2 * bits);
+    { O.y; out_valid }
+  ;;
+
+  let hierarchical ~name ~config scope i =
+    let module H = Hierarchy.In_scope (I) (O) in
+    let { O.y; out_valid = _ } = H.hierarchical ~scope ~name (create ~config) i in
+    y
+  ;;
+end
+
+let create_width_arg bits : (module Width) =
+  (module struct
+    let bits = bits
+  end)
+;;
+
+let hierarchical ?name ~config ~clock ~enable ~scope (x : Signal.t) =
+  let bits = Signal.width x in
+  let module M = With_interface ((val create_width_arg bits)) in
+  let name = Option.value ~default:(Printf.sprintf "square_%d" bits) name in
+  M.hierarchical ~name ~config scope { clock; enable; x; in_valid = vdd }
+;;
--- test/test_half_width_multiplier.ml
+++ test/test_half_width_multiplier.ml
@@ -3,20 +3,18 @@ open! Hardcaml
 open! Snarks_r_fun
 open! Bits
 
-module Half_width_multiplier377 = Half_width_multiplier.With_interface_multiply (struct
+module Squarer_377 = Squarer.With_interface (struct
   let bits = 377
 end)
 
-module Config = Half_width_multiplier.Config
-module I = Half_width_multiplier377.I
-module O = Half_width_multiplier377.O
+module Config = Squarer.Config
+module I = Squarer_377.I
+module O = Squarer_377.O
 module Sim = Cyclesim.With_interface (I) (O)
 
 let create_sim ~config =
   let scope = Scope.create ~flatten_design:true () in
-  Sim.create
-    ~config:Cyclesim.Config.trace_all
-    (Half_width_multiplier377.create ~config scope)
+  Sim.create ~config:Cyclesim.Config.trace_all (Squarer_377.create ~config scope)
 ;;
 
 let rand () = Utils.random_z ~lo_incl:Z.zero ~hi_incl:Z.((of_int 2 ** 377) - one)
@@ -33,35 +31,30 @@ let%expect_test "" =
   inputs.enable := Bits.vdd;
   let test_cases =
     List.concat
-      [ [ Z.zero, max_value; max_value, max_value; max_value, Z.one ]
-      ; List.init 50 ~f:(fun _ -> rand (), rand ())
+      [ [ Z.zero; max_value; Z.one; Z.of_int 2; Z.of_int 42; Z.(max_value - one) ]
+      ; List.init 50 ~f:(fun _ -> rand ())
       ]
   in
   let obtained_results = Queue.create () in
   let cycle () =
     Cyclesim.cycle sim;
     if is_vdd !(outputs.out_valid)
-    then Queue.enqueue obtained_results (Bits.to_z ~signedness:Unsigned !(outputs.z))
+    then Queue.enqueue obtained_results (Bits.to_z ~signedness:Unsigned !(outputs.y))
   in
   inputs.in_valid := vdd;
-  List.iter test_cases ~f:(fun (a, b) ->
-      inputs.x := bits_of_z a;
-      inputs.y := bits_of_z b;
+  List.iter test_cases ~f:(fun x ->
+      inputs.x := bits_of_z x;
       cycle ());
   inputs.in_valid := gnd;
   for _ = 1 to Config.latency config do
     cycle ()
   done;
   assert (Queue.length obtained_results = List.length test_cases);
-  List.iter2_exn (Queue.to_list obtained_results) test_cases ~f:(fun obtained (x, y) ->
-      let expected = Z.(x * y land max_value) in
+  List.iter2_exn (Queue.to_list obtained_results) test_cases ~f:(fun obtained x ->
+      let expected = Z.(x * x) in
       if not (Z.equal obtained expected)
       then
         raise_s
           [%message
-            "Test case failed!"
-              (x : Utils.z)
-              (y : Utils.z)
-              (obtained : Utils.z)
-              (expected : Utils.z)])
+            "Test case failed!" (x : Utils.z) (obtained : Utils.z) (expected : Utils.z)])
 ;;
