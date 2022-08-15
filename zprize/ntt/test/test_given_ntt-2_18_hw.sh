3f1d12f7ac4c9ad5fa5c6d2946c37c136a454b3d
Fu Yong Quah <fyquah@protonmail.com>
2022-08-15 08:58:43 -0400

    Add scripts to test against given input and output files

---
+++ zprize/ntt/test/test_given_ntt-2_18_hw.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export INPUT_FILENAME="$1"
+export EXPECTED_OUTPUT_FILENAME="$2"
+export OUTPUT_FILENAME="/tmp/ntt-fpga-evaluate-given-output.txt"
+
+make -C ../host evaluate_given.exe
+cp ../host/evaluate_given.exe ./
+
+# xsim requires xrt.ini requires absolute directory for pre-run tcl scripts.
+sed -e "s#CURRENT_DIRECTORY#$PWD#g" xrt.template.ini >xrt.ini
+
+../host/evaluate_given.exe  \
+    --xclbin ../fpga/ntt-2_18/build/build_dir.hw.xilinx_u55n_gen3x4_xdma_2_202110_1/ntt_fpga.xclbin \
+    --core-type NTT-2_18 \
+    --input-filename "$INPUT_FILENAME" \
+    --output-filename "$OUTPUT_FILENAME"
+
+diff "$OUTPUT_FILENAME" "$EXPECTED_OUTPUT_FILENAME"
+
+# if the diff returns 0, it means it is is empty and the test succeeded.
+echo "Test succeeded!"
