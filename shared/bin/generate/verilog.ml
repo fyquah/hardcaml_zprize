5c8d13e7021d3b08529006635167da3a83e07608
fyquah <fyquah@protonmail.com>
2022-08-28 23:02:45 +0100

    Add barrett reduction fpga syntheis target and constraint file.

--- shared/bin/generate/verilog.ml
+++ shared/bin/generate/verilog.ml
@@ -10,6 +10,10 @@ module Montgometry_mult377 = Montgomery_mult.With_interface (struct
   let bits = 377
 end)
 
+module Barrett_reduction377 = Barrett_reduction.With_interface (struct
+  let bits = 377
+end)
+
 module Ec_fpn_dbl = Ec_fpn_dbl.With_interface (struct
   let bits = 377
 end)
@@ -174,6 +178,25 @@ let command_montgomery_mult =
        Rtl.output ~database ~output_mode:(To_file filename) Verilog circuit)
 ;;
 
+let command_barrett_reduction =
+  Command.basic
+    ~summary:"Barrett Reduction"
+    (let%map_open.Command filename = flag_filename in
+     fun () ->
+       let module B = Barrett_reduction377 in
+       let module C = Circuit.With_interface (B.I) (B.O) in
+       let scope = Scope.create ~flatten_design:false () in
+       let database = Scope.circuit_database scope in
+       let circuit =
+         B.create
+           ~config:Config_presets.For_bls12_377.barrett_reduction_config
+           ~p:(Ark_bls12_377_g1.modulus ())
+           scope
+         |> C.create_exn ~name:"barrett_reduction"
+       in
+       Rtl.output ~database ~output_mode:(To_file filename) Verilog circuit)
+;;
+
 let command_point_double =
   Command.basic
     ~summary:"point double"
@@ -277,6 +300,7 @@ let () =
   ; "verilog-43x43-multiplier", command_verilog_43x43_multiplier
   ; "karatsuba-ofman-mult", command_karatsuba_ofman_mult
   ; "montgomery-mult", command_montgomery_mult
+  ; "barrett-reduction", command_barrett_reduction
   ; "point-double", command_point_double
   ; "point-add", command_point_add
   ]
