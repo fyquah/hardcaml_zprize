4b01649348c03a255a9d5e047b81acc6d6a93f72
fyquah <fyquah@protonmail.com>
2022-08-28 12:34:07 +0100

    Expose multiply and reduce functions in shared/src/config_presets

--- shared/src/config_presets.mli
+++ shared/src/config_presets.mli
@@ -1,4 +1,7 @@
 module For_bls12_377 : sig
+  val multiply : Ec_fpn_ops_config.fn
+  val montgomery_reduce : Ec_fpn_ops_config.fn
+  val barrett_reduce : Ec_fpn_ops_config.fn
   val montgomery_reduction_config : Montgomery_reduction.Config.t
   val barrett_reduction_config : Barrett_reduction.Config.t
   val ec_fpn_ops_with_barrett_reduction : Ec_fpn_ops_config.t
