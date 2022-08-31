d6406dc33c66e17376bf7b8925d49b4088304bd9
fyquah <fyquah@protonmail.com>
2022-08-31 18:46:43 +0100

    Tune twisted mixed add resources' usage.
    Bias towards LUTs in the multiply by constant, and DSP in the multiply
    between unknowns.
    
    This results in a clear net win in resources. By using raw multiplies,
    we crawl back some latency too.
    
    Synthesis results on twisted mixed adder
    ----
    
    before:
        LUTs: 175896
        FF: 177629
        DSPs: 1544
    
    after:
        LUTs: 172065
        FF: 157320
        DSPs: 1460

--- elliptic_curve/test/test_ec_fpn_mixed_add_with_montgomery_mult.ml
+++ elliptic_curve/test/test_ec_fpn_mixed_add_with_montgomery_mult.ml
@@ -7,7 +7,7 @@ let latency = Ec_fpn_mixed_add.latency config
 
 let%expect_test "latency" =
   Stdio.printf "latency = %d\n" latency;
-  [%expect {| latency = 173 |}]
+  [%expect {| latency = 161 |}]
 ;;
 
 let%expect_test "Test on some test cases" =
