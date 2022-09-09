d3bc66178b4a01dcd4f58360741e65595eac2daa
fyquah <fyquah@protonmail.com>
2022-09-09 18:36:35 +0100

    Don't generate too many points

--- zprize/msm_pippenger/test_harness/src/util.rs
+++ zprize/msm_pippenger/test_harness/src/util.rs
@@ -12,12 +12,11 @@ pub fn generate_points_scalars<G: AffineCurve>(
     len: usize,
     batch_size: usize
 ) -> (Vec<G>, Vec<G::ScalarField>) {
-    let rand_gen: usize = 1 << 26;
     let mut rng = ChaCha20Rng::from_entropy();
 
     let mut points =
         <G::Projective as ProjectiveCurve>::batch_normalization_into_affine(
-            &(0..rand_gen)
+            &(0..len)
                 .map(|_| G::Projective::rand(&mut rng))
                 .collect::<Vec<_>>(),
         );
