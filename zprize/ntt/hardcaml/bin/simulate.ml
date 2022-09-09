a7e5699504899e11b2210db78d6332b8ea0152fc
Andrew Ray <evilkidder@gmail.com>
2022-09-09 09:13:43 +0100

    Ntt 4step test bench for vitis kernel (#45)
    working vitis kernel sim

--- zprize/ntt/hardcaml/bin/simulate.ml
+++ zprize/ntt/hardcaml/bin/simulate.ml
@@ -31,10 +31,44 @@ let command_kernel =
           | Some filename ->
             let coefs = In_channel.read_lines filename |> Array.of_list in
             Array.init (1 lsl logn) ~f:(fun row ->
-              Array.init (1 lsl logn) ~f:(fun col ->
-                Z.of_string coefs.((row * (1 lsl logn)) + col)))
+                Array.init (1 lsl logn) ~f:(fun col ->
+                    Z.of_string coefs.((row * (1 lsl logn)) + col)))
         in
         let waves = Test.run ~verbose ~waves ~first_4step_pass input_coefs in
+        Option.iter waves ~f:Hardcaml_waveterm_interactive.run]
+;;
+
+let command_kernel_for_vitis =
+  Command.basic
+    ~summary:"Simulate kernel operation"
+    [%map_open.Command
+      let verbose = flag "-verbose" no_arg ~doc:" Print detailed results"
+      and input_coefs = flag "-inputs" (optional string) ~doc:" Input coefficients"
+      and logn = anon ("LOGN" %: int)
+      and waves = flag "-waves" no_arg ~doc:" Display interactive waveform"
+      and seed = flag "-seed" (optional_with_default 100 int) ~doc:" Random seed" in
+      fun () ->
+        let rand_state = Random.State.make [| seed |] in
+        Random.set_state rand_state;
+        let module Test =
+          Ntts_r_fun_test.Test_kernel_for_vitis.Make (struct
+            let logn = logn
+
+            let twiddle_4step_config : Ntts_r_fun.Ntt.twiddle_4step_config option =
+              Some { rows_per_iteration = 8; log_num_iterations = logn - 3 }
+            ;;
+          end)
+        in
+        let input_coefs =
+          match input_coefs with
+          | None -> Test.random_input_coef_matrix ()
+          | Some filename ->
+            let coefs = In_channel.read_lines filename |> Array.of_list in
+            Array.init (1 lsl logn) ~f:(fun row ->
+                Array.init (1 lsl logn) ~f:(fun col ->
+                    Z.of_string coefs.((row * (1 lsl logn)) + col)))
+        in
+        let waves = Test.run ~verbose ~waves input_coefs in
         Option.iter waves ~f:Hardcaml_waveterm_interactive.run]
 ;;
 
@@ -47,8 +81,8 @@ let command_ntt =
       fun () ->
         let input_coefs =
           Array.init (1 lsl logn) ~f:(fun _ ->
-            let c = Ntts_r_fun.Gf_z.random () in
-            Ntts_r_fun.Gf_z.to_z c |> Ntts_r_fun_test.Test_ntt_hw.Gf.of_z)
+              let c = Ntts_r_fun.Gf_z.random () in
+              Ntts_r_fun.Gf_z.to_z c |> Ntts_r_fun_test.Test_ntt_hw.Gf.of_z)
         in
         let waves, _result =
           Ntts_r_fun_test.Test_ntt_hw.inverse_ntt_test ~waves input_coefs
@@ -60,5 +94,5 @@ let () =
   Command_unix.run
     (Command.group
        ~summary:"NTT Simulations"
-       [ "kernel", command_kernel; "ntt", command_ntt ])
+       [ "kernel", command_kernel; "vitis", command_kernel_for_vitis; "ntt", command_ntt ])
 ;;
--- zprize/ntt/hardcaml/test/test_kernel_for_vitis.mli
+++ zprize/ntt/hardcaml/test/test_kernel_for_vitis.mli
@@ -7,12 +7,10 @@ module Make (Config : Ntts_r_fun.Ntt.Config) : sig
   val print_matrix : Ntts_r_fun.Gf_z.t array array -> unit
   val copy_matrix : 'a array array -> 'a array array
   val get_results : Bits.t list -> Gf_z.t array array
-  val twiddle : Gf_z.t array array -> unit
 
   val run
     :  ?verbose:bool
     -> ?waves:bool
-    -> first_4step_pass:bool
     -> Z.t array array
     -> Hardcaml_waveterm.Waveform.t option
 end
