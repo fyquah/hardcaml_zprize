44ecb787845a4ffadb0bae238c1cad67b01fffd2
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2022-09-11 05:01:52 -0400

    add benchmark

--- zprize/msm_pippenger/host/benchmark/bench_point_arithmetic.cpp
+++ zprize/msm_pippenger/host/benchmark/bench_point_arithmetic.cpp
@@ -1,3 +1,4 @@
+#include <random>
 #include <benchmark/benchmark.h>
 #include "./utils.h"
 #include "bls12_377_g1/bls12_377_g1.h"
@@ -16,4 +17,20 @@ static void bench_point_arithmetic(benchmark::State &state) {
   }
 }
 
+static void bench_twisted_edwards_to_weierstrass(benchmark::State &state) {
+  bls12_377_g1::init();
+  uint32_t RAND_WORDS[4][bls12_377_g1::NUM_32B_WORDS];
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < bls12_377_g1::NUM_32B_WORDS; j++) {
+      RAND_WORDS[i][j] = std::rand() % (1 << 29);
+    }
+  }
+  bls12_377_g1::Xyzt a(RAND_WORDS[0], RAND_WORDS[1], RAND_WORDS[2], RAND_WORDS[3]);
+
+  for (auto _ : state) {
+    a.extendedTwistedEdwardsToWeierstrass();
+  }
+}
+
 BENCHMARK(bench_point_arithmetic)->UseRealTime();
+BENCHMARK(bench_twisted_edwards_to_weierstrass)->UseRealTime();
