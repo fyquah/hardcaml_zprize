0fb5582aa0d68352e6575842d9b92267e01e2ca3
Fu Yong Quah <fyquah@protonmail.com>
2022-09-17 10:42:14 +0100

    m

--- zprize/msm_pippenger/hardcaml/test/components/test_compact_stream.ml
+++ zprize/msm_pippenger/hardcaml/test/components/test_compact_stream.ml
@@ -29,13 +29,23 @@ let%expect_test "" =
   [%expect {| ((num_256_words_per_point 6) (num_512_words_per_point 3)) |}]
 ;;
 
-let debug = true
+let debug = false
 
-type data_and_last =
-  { data : Bits.t list
-  ; last : bool
-  }
-[@@deriving equal, sexp_of]
+module Input_data = struct
+  type t =
+    { data : Bits.t list
+    ; last : bool
+    }
+  [@@deriving equal, sexp_of]
+end
+
+module Output_for_a_cycle = struct
+  type t =
+    { tdata : Bits.t
+    ; tlast : bool
+    }
+  [@@deriving equal, sexp_of]
+end
 
 let test ~num_points ~probability_up_tvalid ~probability_dn_tready =
   let waves, sim = Waveform.create (create_sim ()) in
@@ -54,16 +64,19 @@ let test ~num_points ~probability_up_tvalid ~probability_dn_tready =
     then
       Accumulator.push
         accumulator
-        (!(outputs_before.dn.tdata), Bits.to_bool !(outputs_before.dn.tlast))
+        { Output_for_a_cycle.tdata = !(outputs_before.dn.tdata)
+        ; tlast = Bits.to_bool !(outputs_before.dn.tlast)
+        }
   in
-  let input_data =
+  let input_datas =
     List.init num_points ~f:(fun _ ->
-      { data = List.init num_512_words_per_point ~f:(fun _ -> Bits.random ~width:512)
+      { Input_data.data =
+          List.init num_512_words_per_point ~f:(fun _ -> Bits.random ~width:512)
       ; last = Random.bool ()
       })
   in
   inputs.up.tvalid := Bits.gnd;
-  List.iter input_data ~f:(fun point ->
+  List.iter input_datas ~f:(fun point ->
     List.iteri point.data ~f:(fun i tdata ->
       while not (random_bool ~p_true:probability_up_tvalid) do
         cycle ()
@@ -82,27 +95,40 @@ let test ~num_points ~probability_up_tvalid ~probability_dn_tready =
     cycle ()
   done;
   let expected_output_data =
-    List.concat_map input_data ~f:(fun v ->
+    List.concat_map input_datas ~f:(fun v ->
       List.concat_mapi v.data ~f:(fun i x ->
         let btm = Bits.sel_bottom x 256 in
         let top = Bits.sel_top x 256 in
         if i = num_512_words_per_point - 1
         then
+          (* This is the last 512-bit word that needs to be transmitted. *)
           if num_256_words_per_point % 2 = 0
-          then [ btm, false; top, v.last ]
-          else [ btm, v.last ]
-        else [ btm, false; top, false ]))
+          then
+            (* We need both words to fit 256-bit words, so transmit both. Only
+             * the last cycle will have tlast. *)
+            [ { Output_for_a_cycle.tdata = btm; tlast = false }
+            ; { Output_for_a_cycle.tdata = top; tlast = v.last }
+            ]
+          else
+            (* We only need to transmit the bottom 256 bit words. assert tlast
+             * appropriately.
+             *)
+            [ { Output_for_a_cycle.tdata = btm; tlast = v.last } ]
+        else
+          [ { Output_for_a_cycle.tdata = btm; tlast = false }
+          ; { Output_for_a_cycle.tdata = top; tlast = false }
+          ]))
   in
   let obtained_output_data = Accumulator.dump accumulator in
   if debug then Waveform.Serialize.marshall waves "a.hardcamlwaveform";
-  if not ([%equal: (Bits.t * bool) list] obtained_output_data expected_output_data)
+  if not ([%equal: Output_for_a_cycle.t list] obtained_output_data expected_output_data)
   then
     raise_s
       [%message
         "Data mismatch"
-          (input_data : data_and_last list)
+          (input_datas : Input_data.t list)
           (List.zip_exn expected_output_data obtained_output_data
-            : ((Bits.t * bool) * (Bits.t * bool)) list)]
+            : (Output_for_a_cycle.t * Output_for_a_cycle.t) list)]
 ;;
 
 let%expect_test "tvalid always high, tready always high" =
