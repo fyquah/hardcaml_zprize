f1db6ae5e62da5d7fac06fbcb5d9b041e7798529
andrewray <evilkidder@gmail.com>
2022-09-17 08:01:04 +0100

    _

---
+++ zprize/ntt/hardcaml/src/for_vitis.mli
@@ -0,0 +1,31 @@
+open! Base
+open Hardcaml
+
+module Make (Config : Hardcaml_ntt.Ntt_4step.Config) : sig
+  module Ntt_4step : module type of Hardcaml_ntt.Ntt_4step.Make (Config)
+  module Axi_stream = Ntt_4step.Axi_stream
+  module Transposer = Hardcaml_ntt.Transposer
+  module Top : module type of Top.Make (Config)
+
+  module I : sig
+    type 'a t =
+      { ap_clk : 'a
+      ; ap_rst_n : 'a
+      ; controller_to_compute_phase_1 : 'a Axi_stream.Source.t
+      ; controller_to_compute_phase_2 : 'a Axi_stream.Source.t
+      ; compute_to_controller_dest : 'a Axi_stream.Dest.t
+      }
+    [@@deriving sexp_of, hardcaml]
+  end
+
+  module O : sig
+    type 'a t =
+      { compute_to_controller : 'a Axi_stream.Source.t
+      ; controller_to_compute_phase_1_dest : 'a Axi_stream.Dest.t
+      ; controller_to_compute_phase_2_dest : 'a Axi_stream.Dest.t
+      }
+    [@@deriving sexp_of, hardcaml]
+  end
+
+  val create : build_mode:Build_mode.t -> Scope.t -> Signal.t Interface.Create_fn(I)(O).t
+end
