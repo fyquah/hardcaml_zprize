d39ced4cb9e1ba9995b6223854977fd57a468926
fyquah <fyquah@protonmail.com>
2022-09-19 09:52:53 +0100

    light refactor

--- zprize/msm_pippenger_multi_slr/hardcaml/src/kernel_for_vitis.ml
+++ zprize/msm_pippenger_multi_slr/hardcaml/src/kernel_for_vitis.ml
@@ -41,45 +41,44 @@ module Make (C : Config.S) = struct
     let ap_rst_n = i.ap_rst_n in
     let clock = ap_clk in
     let clear = ~:ap_rst_n in
-    let host_to_fpga_dests =
-      Array.init num_cores ~f:(fun _ -> Axi512.Stream.Dest.Of_signal.wires ())
-    in
-    let { Stream_splitter_512.O.dns = host_to_fpgas; up_dest = host_to_fpga_dest } =
-      Stream_splitter_512.create
-        scope
-        { clock; clear; up = i.host_to_fpga; dn_dests = host_to_fpga_dests }
-    in
-    let sub_fpga_to_host_dests =
-      Array.init num_cores ~f:(fun _ -> Axi512.Stream.Dest.Of_signal.wires ())
-    in
+    let stream_splitter = Stream_splitter_512.O.Of_signal.wires () in
     let sub_kernels =
-      Array.init num_cores ~f:(fun core_index ->
-        Kernel_for_single_instance.hierarchical
-          ~build_mode
-          ~core_index
-          scope
-          { ap_clk
-          ; ap_rst_n
-          ; host_to_fpga = host_to_fpgas.(core_index)
-          ; fpga_to_host_dest = sub_fpga_to_host_dests.(core_index)
-          })
+      Array.init num_cores ~f:(fun _ -> Kernel_for_single_instance.O.Of_signal.wires ())
     in
-    Array.iter2_exn host_to_fpga_dests sub_kernels ~f:(fun d k ->
-      Axi512.Stream.Dest.Of_signal.( <== ) d k.host_to_fpga_dest);
-    let gather_output_stream =
-      Gather_output_stream.hierarchical
-        scope
-        { clock
-        ; clear
-        ; sub_fpga_to_hosts = Array.map sub_kernels ~f:(fun k -> k.fpga_to_host)
-        ; fpga_to_host_dest = i.fpga_to_host_dest
-        }
-    in
-    Array.iter2_exn
-      sub_fpga_to_host_dests
-      gather_output_stream.sub_fpga_to_host_dests
-      ~f:Axi512.Stream.Dest.Of_signal.( <== );
-    { O.fpga_to_host = gather_output_stream.fpga_to_host; host_to_fpga_dest }
+    let gather_output_stream = Gather_output_stream.O.Of_signal.wires () in
+    Stream_splitter_512.O.Of_signal.assign
+      stream_splitter
+      (Stream_splitter_512.create
+         scope
+         { clock
+         ; clear
+         ; up = i.host_to_fpga
+         ; dn_dests = Array.map sub_kernels ~f:(fun k -> k.host_to_fpga_dest)
+         });
+    Array.iteri sub_kernels ~f:(fun core_index sub_kernel ->
+      Kernel_for_single_instance.O.Of_signal.assign
+        sub_kernel
+        (Kernel_for_single_instance.hierarchical
+           ~build_mode
+           ~core_index
+           scope
+           { ap_clk
+           ; ap_rst_n
+           ; host_to_fpga = stream_splitter.dns.(core_index)
+           ; fpga_to_host_dest = gather_output_stream.sub_fpga_to_host_dests.(core_index)
+           }));
+    Gather_output_stream.O.Of_signal.assign
+      gather_output_stream
+      (Gather_output_stream.hierarchical
+         scope
+         { clock
+         ; clear
+         ; sub_fpga_to_hosts = Array.map sub_kernels ~f:(fun k -> k.fpga_to_host)
+         ; fpga_to_host_dest = i.fpga_to_host_dest
+         });
+    { O.fpga_to_host = gather_output_stream.fpga_to_host
+    ; host_to_fpga_dest = stream_splitter.up_dest
+    }
   ;;
 
   let hierarchical ~build_mode scope i =
