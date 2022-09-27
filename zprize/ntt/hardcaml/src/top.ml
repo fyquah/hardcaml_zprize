9c916571e0e922da9003226878a2e125fdc45a4b
Fu Yong Quah <fyquah@protonmail.com>
2022-09-27 03:36:42 -0400

    kernel_frequency=200 and Four_step.create

--- zprize/ntt/hardcaml/src/top.ml
+++ zprize/ntt/hardcaml/src/top.ml
@@ -70,7 +70,7 @@ module Make (Config : Hardcaml_ntt.Core_config.S) = struct
         x
     in
     let cores =
-      Four_step.hierarchy
+      Four_step.create
         ~build_mode
         scope
         { Four_step.I.clock = i.clock
