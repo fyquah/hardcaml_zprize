6ef11f5a734e92ede104bf0e29a1a1d8beef36a2
Shared aws compile box <bsdevlin@gmail.com>
2022-10-01 19:40:31 +0000

    fix pre place, 200MHz

--- zprize/msm_pippenger/fpga/pre_place.tcl
+++ zprize/msm_pippenger/fpga/pre_place.tcl
@@ -1,5 +1,3 @@
 add_cells_to_pblock pblock_dynamic_SLR0 [get_cells -hierarchical *SLR0*]
 add_cells_to_pblock pblock_dynamic_SLR1 [get_cells -hierarchical *SLR1*]
 add_cells_to_pblock pblock_dynamic_SLR2 [get_cells -hierarchical *SLR2*]
-~                                                                                                                                                    
-~                                                                                 
