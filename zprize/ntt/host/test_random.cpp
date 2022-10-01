eb2f4d77085f84cde54cbb1907d9dfceacb355cb
Fu Yong Quah <fyquah@protonmail.com>
2022-10-01 18:03:11 -0400

    fix test_random

--- zprize/ntt/host/test_random.cpp
+++ zprize/ntt/host/test_random.cpp
@@ -80,6 +80,7 @@ run_ntt_test(host_args_t host_args)
             << " Obtained = " << obtained_output[i]
             << std::endl;
           found_mismatch = 1;
+          break;
         }
       }
       if (found_mismatch) {
