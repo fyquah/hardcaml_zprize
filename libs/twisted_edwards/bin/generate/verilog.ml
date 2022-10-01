5d587a6021e9fdeba63cdcca4b64d6c7ffca1e39
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2022-10-01 03:58:02 -0400

    add full adder synth

--- libs/twisted_edwards/bin/generate/verilog.ml
+++ libs/twisted_edwards/bin/generate/verilog.ml
@@ -14,8 +14,9 @@ let flag_filename =
       ~doc:" Output filename (Defaults to stdout)")
 ;;
 
-let command_base_mixed_add_barrett_reduction_arbitrated
+let command_base_mixed_add_barrett_reduction
   (module Adder : Adder_intf.S)
+  ~arbitrated
   ~name
   =
   Command.basic
@@ -24,7 +25,12 @@ let command_base_mixed_add_barrett_reduction_arbitrated
      fun () ->
        let module Adder377 = Adder.Make (Num_bits) in
        let module C = Circuit.With_interface (Adder377.I) (Adder377.O) in
-       let config = Lazy.force Config.For_bls12_377.with_barrett_reduction_arbitrated in
+       let config =
+         Lazy.force
+           (if arbitrated
+           then Config.For_bls12_377.with_barrett_reduction_arbitrated
+           else Config.For_bls12_377.with_barrett_reduction_full)
+       in
        let scope = Scope.create () in
        let create = Adder377.create ~config scope in
        let circuit = C.create_exn ~name create in
@@ -33,14 +39,23 @@ let command_base_mixed_add_barrett_reduction_arbitrated
 ;;
 
 let command_mixed_add_barrett_reduction_arbitrated =
-  command_base_mixed_add_barrett_reduction_arbitrated
+  command_base_mixed_add_barrett_reduction
     (module Twisted_edwards_lib.Mixed_add)
+    ~arbitrated:true
     ~name:"twisted_edwards_mixed_add"
 ;;
 
 let command_mixed_add_precompute_barrett_reduction_arbitrated =
-  command_base_mixed_add_barrett_reduction_arbitrated
+  command_base_mixed_add_barrett_reduction
     (module Twisted_edwards_lib.Mixed_add_precompute)
+    ~arbitrated:true
+    ~name:"twisted_edwards_mixed_add_precompute"
+;;
+
+let command_mixed_add_precompute_barrett_reduction_full =
+  command_base_mixed_add_barrett_reduction_full
+    (module Twisted_edwards_lib.Mixed_add_precompute)
+    ~arbitrated:false
     ~name:"twisted_edwards_mixed_add_precompute"
 ;;
 
@@ -48,6 +63,8 @@ let () =
   [ "twisted-edwards-mixed-add", command_mixed_add_barrett_reduction_arbitrated
   ; ( "twisted-edwards-mixed-add-precompute"
     , command_mixed_add_precompute_barrett_reduction_arbitrated )
+  ; ( "twisted-edwards-mixed-add-precompute-full"
+    , command_mixed_add_precompute_barrett_reduction_full )
   ]
   |> Command.group ~summary:"generate verilog for twisted edwards curve operations"
   |> Command_unix.run
