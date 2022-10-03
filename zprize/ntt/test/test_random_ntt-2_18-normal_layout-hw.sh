22d0a9cbc39f44193887f69699258aaba71e85a8
Fu Yong Quah <fyquah@protonmail.com>
2022-10-03 07:54:59 -0400

    Clean up scripts

--- zprize/ntt/test/test_random_ntt-2_18-normal_layout-hw.sh
+++ zprize/ntt/test/test_random_ntt-2_18-normal_layout-hw.sh
@@ -10,7 +10,7 @@ sed -e "s#CURRENT_DIRECTORY#$PWD#g" xrt.template.ini >xrt.ini
 
 ./test_random.exe \
     --xclbin ../fpga/ntt-2_18-normal_layout/build/build_dir.hw.xilinx_u55n_gen3x4_xdma_2_202110_1/ntt_fpga.xclbin \
-    --num-test-cases 1 \
+    --num-test-cases 10 \
     --core-type NTT-2_18 \
     --log-blocks 0 \
     --memory-layout NORMAL_LAYOUT
