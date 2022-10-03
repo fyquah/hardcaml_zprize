e2b2d37a0de91e9bdc7b56947e34409e1ab7bb5f
Fu Yong Quah <fyquah@protonmail.com>
2022-10-03 15:17:19 +0100

    Add test that sums result to infinity

--- zprize/msm_pippenger/test_fpga_harness/tests/msm.rs
+++ zprize/msm_pippenger/test_fpga_harness/tests/msm.rs
@@ -2,14 +2,20 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-use ark_ff::BigInteger256;
+use ark_bls12_377::FrParameters;
+use ark_bls12_377::G1Affine;
+use ark_ec::AffineCurve;
+use ark_ec::msm::VariableBaseMSM;
 use ark_ec::ProjectiveCurve;
+use ark_ff::BigInteger256;
+use ark_ff::Fp256;
+
+use std::ops::Neg;
+use std::str::FromStr;
+use std::time::SystemTime;
 
 #[allow(unused_imports)]
 use blst_msm::*;
-
-use std::str::FromStr;
-use std::time::SystemTime;
 
 #[test]
 fn msm_correctness() {
@@ -36,5 +42,44 @@ fn msm_correctness() {
                 arkworks_results[b],
                 "assertion failed in round = {} batch = {}", r, b);
         }
+    }
+}
+
+#[test]
+fn msm_with_identity_result() {
+    let batches = 4;
+    let num_points = 1 << 8;
+    let g1_generator = G1Affine::prime_subgroup_generator();
+    let mut points = Vec::new();
+    let mut scalars = Vec::new();
+    for i in 0..(num_points) {
+        if i == 0 {
+            points.push(g1_generator);
+        } else {
+            points.push(g1_generator.neg());
+        }
+    }
+
+    for _i in 0..(batches * num_points) {
+        scalars.push(Fp256::<FrParameters>::new(BigInteger256::from(1)));
+    }
+
+    let mut context = multi_scalar_mult_init(points.as_slice());
+    let msm_results = multi_scalar_mult(&mut context, points.as_slice(), unsafe {
+        std::mem::transmute::<&[_], &[BigInteger256]>(scalars.as_slice())
+    });
+
+    for b in 0..batches {
+        let start = b * points.len();
+        let end = (b + 1) * points.len();
+        let arkworks_result =
+            VariableBaseMSM::multi_scalar_mul(points.as_slice(), unsafe {
+                std::mem::transmute::<&[_], &[BigInteger256]>(&scalars[start..end])
+            }).into_affine();
+
+        assert_eq!(
+            msm_results[b].into_affine(),
+            arkworks_result,
+            "msm_with_identity_result test failed!");
     }
 }
