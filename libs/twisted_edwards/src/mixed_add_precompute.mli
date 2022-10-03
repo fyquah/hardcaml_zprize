46003ed48b7150f96874bebba455db61a5eac8e4
fyquah <fyquah@protonmail.com>
2022-10-03 21:15:09 +0100

    Fix some typos in mixed add precomputation docs

--- libs/twisted_edwards/src/mixed_add_precompute.mli
+++ libs/twisted_edwards/src/mixed_add_precompute.mli
@@ -1,8 +1,8 @@
-(** Fully/Half pipelined mixed addition for {{:https://en.wikipedia.org/wiki/Twisted_Edwards_curve} twisted edwards curve} curve with [a = -1], optimized with precomputation.
+(** Fully/Half pipelined mixed addition for a scaled {{:https://en.wikipedia.org/wiki/Twisted_Edwards_curve} twisted edwards curve} curve (ie: [a = -1]), optimized with precomputation.
 
     This implementation is designed such that one of the points is a running
     sum computed on the FPGA, and the other point is a relatively static
-    affine point being streamed into the host via FPGA.
+    affine point streamed from the host.
 
     This is useful when the affine points are added into the
     running sum for many many times before needing to convert back to the
@@ -14,7 +14,7 @@
     and another in affine coordinates) we want to add into an entirely different
     coordinate system such that we can skip some computation.
 
-    The running sum in the FPGA, usually represented in terms
+    The running sum in the FPGA, which is usually represented in
     {{:https://hyperelliptic.org/EFD/g1p/auto-twisted-extended-1.html} extended
     coordinate system}, is represented as follows
 
@@ -30,7 +30,7 @@
     ]}
 
     Note that this means that the identity element in this coordinate system
-    is no longer just (0, 1, 1, 0), but rather (2, 2, 4, 0). Also note that
+    is no longer just [(0, 1, 1, 0)], but rather [(2, 2, 4, 0)]. Also note that
     [x/z * y/z != t/z] in the new coordinate system.
 
     The relative static point has a completely different representation:
@@ -47,7 +47,15 @@
     Addition between these two newly defined coordinate system is as follows:
 
     {[
-      let add_unified_with_precompute running_sum x_host =
+      let add_unified_with_precompute running_sum static_point =
+        (* [x], [y], [z] and [t] is in the running sum representation
+           coordinate system.
+        *)
+        let { x; y; t; z } = running_sum in
+        (* [x_host], [y_host] and [t_host] is in the static point
+           representation cooridnate system.
+        *)
+        let { x_host; y_host; t_host } = static_point in
         let c_A    = x1 * x_host in
         let c_B    = y1 * y_host in
         let c_C    = t1 * t_host in
@@ -73,7 +81,7 @@
     screen.
 
     This mixed adder uses 1 less resources than the vanilla addition formulae.
-    as requires 7M and 6A (over 7M + 8A + 1*2 + 1*k) at the cost of some
+    as requires 7M and 6A (over [7M + 8A + 1*2 + 1*k]) at the cost of some
     preprocessing and post-processing.
 
     {1 Implementation Details}
