d1808bede84ce9b765c0c0062d84b15563a06642
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2022-10-04 12:35:50 -0400

    mathjax -> katex

--- zprize/msm_pippenger/hardcaml/src/scalar_transformation.mli
+++ zprize/msm_pippenger/hardcaml/src/scalar_transformation.mli
@@ -1,8 +1,10 @@
 (** Module to transform scalars to a signed-digit representation, which is expected
     by the downstream FPGA logic.
 
-    {%html: <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script> %}
+    {%html: <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.2/dist/katex.min.css" integrity="sha384-bYdxxUwYipFNohQlHt0bjN/LCpueqWz13HufFEV1SUatKs1cm4L6fFgCi1jT643X" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.2/dist/katex.min.js" integrity="sha384-Qsn9KnoKISj6dI8g7p1HBlNpVx0I8p1SvlwOldgi3IorMle61nQy4zEahWYtljaz" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.2/dist/contrib/auto-render.min.js" integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05" crossorigin="anonymous"
+        onload="renderMathInElement(document.body);"></script> %}
 
     The main transform currently implemented transforms the input scalar from unsigned digits
     in the range {%html: \([0,2^b-1]\) %} to signed digits in the range {%html: \([-2^{b-1}, 2^{b-1}-1]\) %}.
